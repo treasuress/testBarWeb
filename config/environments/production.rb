658323ffb3d59fbe45aa3d4f0c3b9b5336a67f25
auroral <laura@ANG3L-PROJS.(none)>
2014-05-14 17:39:56 -0300

    correcao - test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,12 @@ TestBarWeb::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  config.assets.initialize_on_precompile = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
@@ -46,9 +48,9 @@ TestBarWeb::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  #config.assets.precompile += %w( active_admin.css
-   #                               active_admin.js
-    #                            )
+  config.assets.precompile += %w( active_admin.css
+                                  active_admin.js
+                                )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
