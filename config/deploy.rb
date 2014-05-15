6d2b0716999a76a533a8c54d433a94f7d7726c7d
auroral <laura@ANG3L-PROJS.(none)>
2014-05-15 15:40:28 -0300

    correcao - test

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ load "config/recipes/check"
 
 server "192.168.0.105", :web, :app, :db, primary: true
 
+
 set :user, "deployer"
 set :application, "testBarWeb"
 set :deploy_to, "/home/#{user}/apps/#{application}"
@@ -31,6 +32,14 @@ namespace :deploy do
   task :seed do
     run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
   end
+end
+
+def run_remote_rake(rake_cmd)
+  rake_args = ENV['RAKE_ARGS'].to_s.split(',')
+  cmd = "cd #{fetch(:latest_release)} && #{fetch(:rake, "rake")} RAILS_ENV=#{fetch(:rails_env, "production")} #{rake_cmd}"
+  cmd += "['#{rake_args.join("','")}']" unless rake_args.empty?
+  run cmd
+  set :rakefile, nil if exists?(:rakefile)
 end
 
 def press_enter( ch, stream, data)
